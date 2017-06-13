7ab7baca57175a3f08d87213959c03c361487bf4
baristaGeek <estebanvargas94@gmail.com>
2017-06-13 17:09:49 -0500

    All a1_q1 problems until the Huber loss function problem

--- a1_q1_exercises.py
+++ a1_q1_exercises.py
@@ -14,7 +14,11 @@ import tensorflow as tf
 
 x = tf.random_uniform([])  # Empty array as shape creates a scalar.
 y = tf.random_uniform([])
-out = tf.cond(tf.less(x, y), lambda: tf.add(x, y), lambda: tf.sub(x, y))
+out = tf.cond(tf.less(x, y), lambda: tf.add(x, y), lambda: tf.subtract(x, y))
+
+#with tf.Session() as sess:
+#    out = sess.run([out])
+#    print(out)
 
 ###############################################################################
 # 1b: Create two 0-d tensors x and y randomly selected from -1 and 1.
@@ -22,7 +26,14 @@ out = tf.cond(tf.less(x, y), lambda: tf.add(x, y), lambda: tf.sub(x, y))
 # Hint: Look up tf.case().
 ###############################################################################
 
-# YOUR CODE
+x = tf.random_uniform(shape = [], minval = -1.0, maxval = 1.0)
+y = tf.random_uniform(shape = [], minval = -1.0, maxval = 1.0)
+out = tf.case({tf.less(x, y): lambda: tf.add(x, y), tf.greater(x, y): lambda: tf.subtract(x, y)},
+                default = lambda: tf.constant(0.0), exclusive = True)
+
+#with tf.Session() as sess:
+#    out = sess.run([out])
+#    print(out)
 
 ###############################################################################
 # 1c: Create the tensor x of the value [[0, -2, -1], [0, 1, 2]]
@@ -31,7 +42,13 @@ out = tf.cond(tf.less(x, y), lambda: tf.add(x, y), lambda: tf.sub(x, y))
 # Hint: Look up tf.equal().
 ###############################################################################
 
-# YOUR CODE
+x = tf.constant([[0, -2, 1], [0, 1, 2]])
+y = tf.zeros_like(x)
+out = tf.equal(x, y)
+
+#with tf.Session() as sess:
+#    out = sess.run([out])
+#    print(out)
 
 ###############################################################################
 # 1d: Create the tensor x of value
@@ -46,7 +63,17 @@ out = tf.cond(tf.less(x, y), lambda: tf.add(x, y), lambda: tf.sub(x, y))
 # Hint: Use tf.gather().
 ###############################################################################
 
-# YOUR CODE
+x = tf.constant([29.05088806,  27.61298943,  31.19073486,  29.35532951,
+    30.97266006,  26.67541885,  38.08450317,  20.74983215,
+    34.94445419,  34.45999146,  29.06485367,  36.01657104,
+    27.88236427,  20.56035233,  30.20379066,  29.51215172,
+    33.71149445,  28.59134293,  36.05556488,  28.66994858])
+y = tf.where(x > 30)
+out = tf.gather(x, y)
+
+#with tf.Session() as sess:
+#    out = sess.run([out])
+#    print(out)
 
 ###############################################################################
 # 1e: Create a diagnoal 2-d tensor of size 6 x 6 with the diagonal values of 1,
@@ -54,15 +81,24 @@ out = tf.cond(tf.less(x, y), lambda: tf.add(x, y), lambda: tf.sub(x, y))
 # Hint: Use tf.range() and tf.diag().
 ###############################################################################
 
-# YOUR CODE
+diagonal = tf.range(1,7,1)
+out = tf.diag(diagonal)
+
+#with tf.Session() as sess:
+#    out = sess.run([out])
+#    print(out)
 
 ###############################################################################
 # 1f: Create a random 2-d tensor of size 10 x 10 from any distribution.
 # Calculate its determinant.
 # Hint: Look at tf.matrix_determinant().
 ###############################################################################
+x = tf.random_uniform(shape = [10, 10], minval = -2, maxval = 4)
+out = tf.matrix_determinant(x)
 
-# YOUR CODE
+#with tf.Session() as sess:
+#    out = sess.run([out])
+#    print(out)
 
 ###############################################################################
 # 1g: Create tensor x with value [5, 2, 3, 5, 10, 6, 2, 3, 4, 2, 1, 1, 0, 9].
@@ -70,7 +106,12 @@ out = tf.cond(tf.less(x, y), lambda: tf.add(x, y), lambda: tf.sub(x, y))
 # Hint: use tf.unique(). Keep in mind that tf.unique() returns a tuple.
 ###############################################################################
 
-# YOUR CODE
+x = tf.constant([5, 2, 3, 5, 10, 6, 2, 3, 4, 2, 1, 1, 0, 9])
+out, idx = tf.unique(x)
+
+#with tf.Session() as sess:
+#    out = sess.run([out])
+#    print(out)
 
 ###############################################################################
 # 1h: Create two tensors x and y of shape 300 from any normal distribution,
@@ -82,88 +123,8 @@ out = tf.cond(tf.less(x, y), lambda: tf.add(x, y), lambda: tf.sub(x, y))
 # Hint: see the Huber loss function in the lecture slides 3.
 ###############################################################################
 
-# YOUR CODE"""
-Simple TensorFlow exercises
-You should thoroughly test your code
-"""
+out = tf.random_normal
 
-import tensorflow as tf
-
-###############################################################################
-# 1a: Create two random 0-d tensors x and y of any distribution.
-# Create a TensorFlow object that returns x + y if x > y, and x - y otherwise.
-# Hint: look up tf.cond()
-# I do the first problem for you
-###############################################################################
-
-x = tf.random_uniform([])  # Empty array as shape creates a scalar.
-y = tf.random_uniform([])
-out = tf.cond(tf.less(x, y), lambda: tf.add(x, y), lambda: tf.sub(x, y))
-
-###############################################################################
-# 1b: Create two 0-d tensors x and y randomly selected from -1 and 1.
-# Return x + y if x < y, x - y if x > y, 0 otherwise.
-# Hint: Look up tf.case().
-###############################################################################
-
-# YOUR CODE
-
-###############################################################################
-# 1c: Create the tensor x of the value [[0, -2, -1], [0, 1, 2]]
-# and y as a tensor of zeros with the same shape as x.
-# Return a boolean tensor that yields Trues if x equals y element-wise.
-# Hint: Look up tf.equal().
-###############################################################################
-
-# YOUR CODE
-
-###############################################################################
-# 1d: Create the tensor x of value
-# [29.05088806,  27.61298943,  31.19073486,  29.35532951,
-#  30.97266006,  26.67541885,  38.08450317,  20.74983215,
-#  34.94445419,  34.45999146,  29.06485367,  36.01657104,
-#  27.88236427,  20.56035233,  30.20379066,  29.51215172,
-#  33.71149445,  28.59134293,  36.05556488,  28.66994858].
-# Get the indices of elements in x whose values are greater than 30.
-# Hint: Use tf.where().
-# Then extract elements whose values are greater than 30.
-# Hint: Use tf.gather().
-###############################################################################
-
-# YOUR CODE
-
-###############################################################################
-# 1e: Create a diagnoal 2-d tensor of size 6 x 6 with the diagonal values of 1,
-# 2, ..., 6
-# Hint: Use tf.range() and tf.diag().
-###############################################################################
-
-# YOUR CODE
-
-###############################################################################
-# 1f: Create a random 2-d tensor of size 10 x 10 from any distribution.
-# Calculate its determinant.
-# Hint: Look at tf.matrix_determinant().
-###############################################################################
-
-# YOUR CODE
-
-###############################################################################
-# 1g: Create tensor x with value [5, 2, 3, 5, 10, 6, 2, 3, 4, 2, 1, 1, 0, 9].
-# Return the unique elements in x
-# Hint: use tf.unique(). Keep in mind that tf.unique() returns a tuple.
-###############################################################################
-
-# YOUR CODE
-
-###############################################################################
-# 1h: Create two tensors x and y of shape 300 from any normal distribution,
-# as long as they are from the same distribution.
-# Use tf.less() and tf.select() to return:
-# - The mean squared error of (x - y) if the average of all elements in (x - y)
-#   is negative, or
-# - The sum of absolute value of all elements in the tensor (x - y) otherwise.
-# Hint: see the Huber loss function in the lecture slides 3.
-###############################################################################
-
-# YOUR CODE
+with tf.Session() as sess:
+    out = sess.run([out])
+    print(out)
